2bbfe7c467152b2b9b0e5687a6a3deac8acd17d9
Adilhan <adilhan@MacBook-Air-Adilhan.local>
2024-12-12 20:59:10 +0600

    Создал класс CoinAcceptorCard.java для карты.

---
+++ src/model/CoinAcceptorCard.java
@@ -0,0 +1,17 @@
+package model;
+
+public class CoinAcceptorCard {
+    private int amountCard;
+
+    public CoinAcceptorCard(int amountCard) {
+        this.amountCard = amountCard;
+    }
+
+    public int getAmountCard() {
+        return amountCard;
+    }
+
+    public void setAmountCard(int amountCard) {
+        this.amountCard = amountCard;
+    }
+}
