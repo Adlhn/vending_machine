8c7478fc0edc61adbec7dcce273df2d4634db373
adilhan <esenalievaadilhan@gmail.com>
2024-12-18 19:33:08 +0600

    commit

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -11,6 +11,8 @@ public class AppRunner {
 
     private final CoinAcceptor coinAcceptor;
 
+    private final CoinAcceptorCard coinAcceptorCard;
+
     private static boolean isExit = false;
 
     private AppRunner() {
@@ -23,12 +25,29 @@ public class AppRunner {
                 new Pistachios(ActionLetter.G, 130)
         });
         coinAcceptor = new CoinAcceptor(100);
+        coinAcceptorCard = new CoinAcceptorCard(110);
     }
 
     public static void run() {
+        Scanner scanner = new Scanner(System.in);
         AppRunner app = new AppRunner();
+        System.out.println("Выберите способ оплаты.");
         while (!isExit) {
-            app.startSimulation();
+            System.out.println("1 - Наличка.");
+            System.out.println("2 - Картой.");
+          if (scanner.hasNextInt()) {
+              int choise = scanner.nextInt();
+              if (choise == 1) {
+                  app.startSimulation();
+              } else if (choise == 2) {
+                  app.startSimulationCard();
+              } else {
+                  System.out.println("Выберите способ оплаты");
+              }
+          } else {
+              System.out.println("Выберите способ оплаты");
+              scanner.next();
+          }
         }
     }
 
@@ -41,7 +60,17 @@ public class AppRunner {
         UniversalArray<Product> allowProducts = new UniversalArrayImpl<>();
         allowProducts.addAll(getAllowedProducts().toArray());
         chooseAction(allowProducts);
+    }
 
+    private void startSimulationCard() {
+        print("В автомате доступны:");
+        showProducts(products);
+
+        print("На карте: " + coinAcceptorCard.getAmountCard());
+
+        UniversalArray<Product> allowProductsCard = new UniversalArrayImpl<>();
+        allowProductsCard.addAll(getAllowedProductsCard().toArray());
+        chooseActionCard(allowProductsCard);
     }
 
     private UniversalArray<Product> getAllowedProducts() {
@@ -52,6 +81,16 @@ public class AppRunner {
             }
         }
         return allowProducts;
+    }
+
+    private UniversalArray<Product> getAllowedProductsCard() {
+        UniversalArray<Product> allowProductsCard = new UniversalArrayImpl<>();
+        for (int i = 0; i < products.size(); i++) {
+            if (coinAcceptorCard.getAmountCard() >= products.get(i).getPrice()) {
+                allowProductsCard.add(products.get(i));
+            }
+        }
+        return allowProductsCard;
     }
 
     private void chooseAction(UniversalArray<Product> products) {
@@ -80,8 +119,34 @@ public class AppRunner {
                 chooseAction(products);
             }
         }
+    }
 
-
+    private void chooseActionCard(UniversalArray<Product> products) {
+        print(" a - Пополнить баланс");
+        showActions(products);
+        print(" h - Выйти");
+        String action = fromConsole().substring(0, 1);
+        if ("a".equalsIgnoreCase(action)) {
+            coinAcceptorCard.setAmountCard(coinAcceptorCard.getAmountCard() + 10);
+            print("Вы пополнили баланс на 10");
+            return;
+        }
+        try {
+            for (int i = 0; i < products.size(); i++) {
+                if (products.get(i).getActionLetter().equals(ActionLetter.valueOf(action.toUpperCase()))) {
+                    coinAcceptorCard.setAmountCard(coinAcceptorCard.getAmountCard() - products.get(i).getPrice());
+                    print("Вы купили " + products.get(i).getName());
+                    break;
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            if ("h".equalsIgnoreCase(action)) {
+                isExit = true;
+            } else {
+                print("Недопустимая буква. Попрбуйте еще раз.");
+                chooseAction(products);
+            }
+        }
     }
 
     private void showActions(UniversalArray<Product> products) {
